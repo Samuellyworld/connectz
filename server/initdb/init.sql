744ced445b25b20eba3867877dbb921eb629c8b6
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-02-25 00:31:44 +0100

    feat/adding docker and start db SQL scripts 🎉

---
+++ server/initdb/init.sql
@@ -0,0 +1,22 @@
+-- Table for storing users
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  phone VARCHAR(20) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  verification_code VARCHAR(5),
+  verified_email BOOLEAN DEFAULT FALSE,
+  interests TEXT[],
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Table for storing password reset tokens
+CREATE TABLE password_reset_tokens (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  token VARCHAR(100) NOT NULL UNIQUE,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
