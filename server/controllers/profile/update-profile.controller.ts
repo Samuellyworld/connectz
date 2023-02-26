4dac0131afbbc54af4e9c4eba7ff571e34969aee
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-02-26 04:41:40 +0100

    feat/update profile - phone number💻

--- server/controllers/profile/update-profile.controller.ts
+++ server/controllers/profile/update-profile.controller.ts
@@ -8,7 +8,7 @@ import { QueryResult } from "pg";
 import { db } from "../../db/connect";
 
 // import email validate
-import { validateEmail } from "../../validate/validate";
+import { validateEmail, validatePhoneNumber } from "../../validate/validate";
 
 // types
 import { profileQueryTypes } from "../../types/profile.types";
@@ -50,17 +50,17 @@ export const updateUserProfile = async(req:Request | any, res:Response) => {
          // result
          const updateUsername: QueryResult = await db.query(updateUsernameQuery);
          if (updateUsername.rowCount === 0) {
-            return res.status(400).json(
-                {
+            return res.status(400).json({
                  message: 'Failed to update username' 
                 });
          }
-         res.status(200).json(
-             { 
+         //send a response
+         res.status(200).json({ 
                 message: 'Username updated successfully' 
              });
-        } else if (type === "email") {
-
+        } 
+        // if type is email
+        else if (type === "email") {
             // validate email address
           if(!validateEmail(identifier)) {
             return res.status(400).json({
@@ -88,18 +88,62 @@ export const updateUserProfile = async(req:Request | any, res:Response) => {
          // result
          const updateEmail: QueryResult = await db.query(updateEmailQuery);
          if (updateEmail.rowCount === 0) {
-            return res.status(400).json(
-                {
+            return res.status(400).json( {
                  message: 'Failed to update Email' 
                 });
          }
-         res.status(200).json(
-             { 
+         // send a response
+         res.status(200).json({ 
                 message: 'Email updated successfully' 
              });
+        } else if (type === "phone") {
+            // validate phone number
+            if (!validatePhoneNumber(identifier)) {
+             return res.status(400).json({
+                message : "Invalid Phone Number"
+             });
+            }
+           // check if the new PhoneNumber is already in use
+           const checkNewPhoneNumberQuery : profileQueryTypes = {
+            text : 'SELECT * FROM users WHERE Phone = $1',
+            values : [identifier]
+         }
+        // result from db
+         const newPhoneNumberResult : QueryResult = await db.query(checkNewPhoneNumberQuery);
+       // send a response if PhoneNumber exist
+        if(newPhoneNumberResult.rowCount > 0) {
+            return res.status(400).json({
+                message : 'PhoneNumber  is already in use'
+            })
         }
-  
-    } catch (err) {
-        
+         // update the user's PhoneNumber in the database;
+         const updatePhoneNumberQuery : profileQueryTypes = {
+             text :  'UPDATE users SET Phone = $1 WHERE id = $2',
+             values : [identifier, userId]
+         }
+         // result
+         const updatePhoneNumber: QueryResult = await db.query(updatePhoneNumberQuery);
+         if (updatePhoneNumber.rowCount === 0) {
+            return res.status(400).json({
+                 message: 'Failed to update PhoneNumber' 
+                });
+         }
+         // sends a response
+         res.status(200).json({ 
+                message: 'User Phone Number updated successfully' 
+             });
+        } else {
+         // send a error response
+            return res.status(400).json({
+                message : "Error Updating user"
+            })
+        }
+      }
+     // catche error
+     catch (err) {
+        console.log(err.message);
+        res.status(500).json({
+            message : 'server error'
+        });
     }
 }
