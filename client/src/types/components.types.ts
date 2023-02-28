05df999761971a02b8aabd651ead9afed6d2c069
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-02-28 05:50:47 +0100

    fix/fixing and debugging -input validation 🎉

---
+++ client/src/types/components.types.ts
@@ -0,0 +1,8 @@
+// sign up input change types
+export interface signUpInputChangeTypes {
+    username : string,
+    email : string,
+    password : string,
+    confirm_password : string,
+    textReveal : string
+}
