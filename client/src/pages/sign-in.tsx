5093157ff0a58454322a301f0295528e3a60ad40
Aspirin 💆🏽 <samuellyworld@gmail.com>
2023-02-28 04:12:31 +0100

    feat/sign in page , no validation yet 🎈

---
+++ client/src/pages/sign-in.tsx
@@ -0,0 +1,10 @@
+// import relevant component
+import SignIn from "../components/sign-in"
+
+const SignInPage : () => JSX.Element = () => {
+    return (
+        <SignIn/>
+    )
+}
+
+export default SignInPage;
